ce936a463e3508049ac66fc83505289779b5a1bd
Aaron Diggdon <aarondiggdon@gmail.com>
2023-08-31 23:09:03 +0100

    Changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {
-  BrowserRouter as Router,Routes,
+  HashRouter as Router,Routes,
   Route
 } from 'react-router-dom';
 
